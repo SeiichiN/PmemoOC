e0a2642303b4b1693809fd37885654bcc06a6db1
SeiichiN <billie175@gmail.com>
2019-06-06 18:26:04 +0900

    len.ml added.

--- len.ml
+++ len.ml
@@ -1,15 +1,9 @@
 (*
  * len.ml -- 文字列の長さを取得する
  *)
-let s1 = "google"
-and s2 = "itソリューション"
-and s3 = "大阪市立図書館"
-in
-print_endline (s1 ^ "= " ^ string_of_int(String.length s1));
-print_endline (s2 ^ "= " ^ string_of_int(String.length s2));
-print_endline (s3 ^ "= " ^ string_of_int(String.length s3));
-
-print_endline "全角文字の数"
+let s1 = "google";;
+let s2 = "itソリューション";;
+let s3 = "大阪市立図書館";;
 
 (* あ -- \227 \129 \130
  * い -- \227 \129 \132
@@ -17,21 +11,50 @@ print_endline "全角文字の数"
  *)
 
 let t = ref 0
+let n = ref 0
+let zen = ref 0
+let sw = ref false 
 
 let count x =
-    let check () = 
-        if (Char.code x) >= 227
-        then
-            t := !t + 1
-    in
+  n := !n + 1; 
+  let check1 () =
+    if (Char.code x) >= 227
+    then ( sw := true )
+    else ()
+  in
+  check1 ();
+  let check2 () =
+    if !sw = true
+    then (
+      t := !t + 1;
+      zen := !zen + 1 )
+    else ()
+  in
+  check2 ();
+  let check3 () =
+    if !t = 3
+    then (sw := false; t := 0 )
+    else ()
+  in
+  check3 ()
+
 
 
 let mblength s =
-    let n = ref 0 in
-    String.iter (fun x -> n := !n + 1) s; !n
+  sw := false;
+  n := 0;
+  zen := 0;
+  t := 0;
+  String.iter (fun x -> count x) s;
+  Printf.printf "半角文字数 = %d\n" (!n - !zen);
+  Printf.printf "全角文字数 = %d\n" (!zen / 3)
  
-
-
 (*
-        Exception: Invalid_argument "index out of bounds".
-*)
+ * 決められた文字数で文字列を表示する
+ * @param: s : string -- 表示する文字列
+ *         l : int    -- 表示の幅（文字数）
+ * @return: string -- 決められたサイズの文字列
+ *                    サイズの小さな文字列は空白で埋めることとする
+ *)
+let set_length s l =
+  ""
